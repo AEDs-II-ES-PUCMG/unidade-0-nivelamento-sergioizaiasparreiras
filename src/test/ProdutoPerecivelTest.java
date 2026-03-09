5413d2134ac77ec57f19bb0d4fb7b9db59779749
sergioizaiasparreiras <sergioparreiras09@gmail.com>
2026-03-09 20:46:27 -0300

    fix: data valida para o teste ( atualizando para 2026)

--- src/test/ProdutoPerecivelTest.java
+++ src/test/ProdutoPerecivelTest.java
@@ -37,10 +37,10 @@ public class ProdutoPerecivelTest {
 
     @Test
     public void criarCorretamenteAPartirDeTexto(){
-        String linhaDados = "2;Produto perecível do arquivo;10.0;0.2;25/10/2025";
+        String linhaDados = "2;Produto perecível do arquivo;10.0;0.2;25/10/2026";
         produto = Produto.criarDoTexto(linhaDados);
         String desc = produto.toString();
-        assertTrue(desc.contains("Produto perecível do arquivo") && desc.contains("R$ 12,00") && desc.contains("25/10/2025"));
+        assertTrue(desc.contains("Produto perecível do arquivo") && desc.contains("R$ 12,00") && desc.contains("25/10/2026"));
     }
 
     @Test
